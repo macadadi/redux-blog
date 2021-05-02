1373ca542c809dc08f47baed204615bdb72c25df
macadadi <macadadi36@gmail.com>
2021-05-02 12:04:05 +0300

    single author posts

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { SinglePostPage } from './features/posts/SinglePostPage'
 import {EditPostForm} from './features/posts/EditPostForm'
 import {UsersList} from './features/users/UsersList'
 import {UserPage} from './features/users/UserPage'
+import { NotificationsList } from './features/notifications/NotificationsList'
 import {
   BrowserRouter as Router,
   Switch,
@@ -20,6 +21,7 @@ function App() {
       <Navbar />
       <div className="App">
         <Switch>
+        <Route exact path="/notifications" component={NotificationsList} />
           <Route
             exact
             path="/"
